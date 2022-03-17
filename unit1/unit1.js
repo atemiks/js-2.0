038877850e06309b89de41a103fab360e6cb6d8e
atemiks <atemiks@gmail.com>
2022-03-17 19:17:29 +0200

    Unit 1

---
+++ unit1/unit1.js
@@ -0,0 +1,77 @@
+// task 1
+console.log('Artem');
+
+// task 2
+console.log(8);
+
+// task 3
+console.log('Добро' + ' пожаловать ' + 'на курс');
+
+// task 4
+// alert('2019');
+
+// task 5
+// alert(2019-200);
+
+// task 6
+document.querySelector('#one').textContent = 'Hello World';
+
+// task 7
+document.querySelector('#two').textContent = 12 * 12;
+
+// task 8
+document.querySelector('.one').textContent = 'Hello World';
+
+// task 9
+document.querySelector('h2 span').textContent = 'world';
+
+// task 10
+document.querySelector('.three').innerHTML = '<h3>Hello world</h3>';
+
+// task 11
+document.querySelector('.four').innerHTML = '<h4>H4 headline</h4>' + '<p>Lorem ipsum</p>';
+
+// task 12
+let a = document.querySelector('.five');
+a.innerHTML = 'a = 3.1415';
+
+// task 13
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/187_Js_logo_logos-512.png" width="48" height"48" alt="JavaScript logo">';
+
+// task 14
+let z1 = 6,
+    z2 = 3;
+
+document.querySelector('.eight').innerHTML = 'z1 * z2 = ' + ' ' + z1 * z2;
+
+// task 15
+let y1 = 6,
+    y2 = 3;
+
+document.querySelector('.nine').innerHTML = 'y1 / y2 = ' + ' ' + y1 / y2;
+
+// task 16
+let x1 = 'Hello',
+    x2 = 5;
+
+document.querySelector('.ten').innerHTML = 'x1 + x2 = ' + ' ' + x1 + x2;
+
+// task 17
+let d1 = document.querySelector('.test-1 ');
+console.log(d1);
+
+// task 18
+let d2 = document.querySelector('div.test-2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
+
+// task 19
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
+// task 20
+// document.querySelector('body').innerHTML = '';
